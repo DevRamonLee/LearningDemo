ec6ca9a3be46878a28b056be6c70f74d9a246762
meng.li <meng.li@yaok.com>
2019-06-13 15:32:49 +0800

    RN 项目转 Web

--- FrontEnd/hello_react_native/index.web.js
+++ FrontEnd/hello_react_native/index.web.js
@@ -1,5 +1,7 @@
 import { AppRegistry } from 'react-native';
-//import App from './App'; //修改前
 import App from './src/App';
 
 AppRegistry.registerComponent('hello_react_native2', () => App);
+AppRegistry.runApplication('hello_react_native2', {
+    rootTag: document.getElementById('react-root')
+});

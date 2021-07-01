862fbc3e1fa2b810d228201b6d6990cebbb7b43e
Ramon <15900962242@163.com>
2021-07-01 23:44:18 +0800

    [Android][Basic][AndroidUI] option menu/ subMenu / ContextMenu

--- Android/Basic/Core/AndroidUI/app/src/main/java/ramon/lee/androidui/interactive/MenuActivity.java
+++ Android/Basic/Core/AndroidUI/app/src/main/java/ramon/lee/androidui/interactive/MenuActivity.java
@@ -1,8 +1,12 @@
 package ramon.lee.androidui.interactive;
 
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.SubMenu;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,11 +23,20 @@ public class MenuActivity extends AppCompatActivity {
     private static final int menu5 = 5;
     private static final int menu6 = 6;
     private static final int menu7 = 7;
+    private static final int subMenu1 = 8;
+    private static final int subMenu2 = 9;
 
+
+    private Button btnContextMenu1;
+    private Button btnContextMenu2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        btnContextMenu1 = findViewById(R.id.btn_show_context_menu1);
+        btnContextMenu2 = findViewById(R.id.btn_show_context_menu2);
+        registerForContextMenu(btnContextMenu1);
+        registerForContextMenu(btnContextMenu2);
     }
 
 
@@ -39,6 +52,10 @@ public class MenuActivity extends AppCompatActivity {
      */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        SubMenu M3 = menu.addSubMenu("Menu0");
+        M3.add(0, subMenu1, 0, "SubMenu1");
+        M3.add(0, subMenu2, 0, "SubMenu2");
+
         menu.add(0, menu1, 0, "Menu1");
         menu.add(0, menu2, 0, "Menu2");
         menu.add(0, menu3, 0, "Menu3");
@@ -72,6 +89,58 @@ public class MenuActivity extends AppCompatActivity {
                 break;
             case menu7:
                 Toast.makeText(this, "你点击Menu7", Toast.LENGTH_LONG).show();
+                break;
+            case subMenu1:
+                Toast.makeText(this, "你点击SubMenu1", Toast.LENGTH_LONG).show();
+                break;
+            case subMenu2:
+                Toast.makeText(this, "你点击SubMenu2", Toast.LENGTH_LONG).show();
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        if(v.getId() == R.id.btn_show_context_menu1) {
+            menu.add(0, menu1, 0, "Menu1");
+            menu.add(0, menu2, 0, "Menu2");
+            menu.add(0, menu3, 0, "Menu3");
+        } else if (v.getId() == R.id.btn_show_context_menu2) {
+            menu.add(0, menu4, 0, "Menu4");
+            menu.add(0, menu5, 0, "Menu5");
+            menu.add(0, menu6, 0, "Menu6");
+        }
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case menu1:
+                Toast.makeText(this, "你 点击的是Menu1", Toast.LENGTH_LONG).show();
+                btnContextMenu1.setText("你点击的是Menu1");
+                break;
+
+            case menu2:
+                Toast.makeText(this, "你点击的是Menu2", Toast.LENGTH_LONG).show();
+                btnContextMenu1.setText("你点击的是Menu2");
+                break;
+            case menu3:
+                Toast.makeText(this, "你点击的是Menu3", Toast.LENGTH_LONG).show();
+                btnContextMenu1.setText("你点击的是Menu3");
+                break;
+            case menu4:
+                Toast.makeText(this, "你点击的是Menu4", Toast.LENGTH_LONG).show();
+                btnContextMenu2.setText("你点击的是Menu4");
+                break;
+            case menu5:
+                Toast.makeText(this, "你点击的是Menu5", Toast.LENGTH_LONG).show();
+                btnContextMenu2.setText("你点击的是Menu5");
+                break;
+            case menu6:
+                Toast.makeText(this, "你点击的是Menu6", Toast.LENGTH_LONG).show();
+                btnContextMenu2.setText("你点击的是Menu6");
                 break;
         }
         return true;

31608f13f44b23475e01b348bcebf784a817ff9d
Ramon <ramon@192.168.1.8>
2024-02-01 22:53:46 +0800

    [Andrid] update four components

--- Harmony/AtomicServiceNews/entry/src/main/java/com/example/atomicservicenews/distribute/api/SelectDeviceResultListener.java
+++
@@ -1,8 +0,0 @@
-package com.example.atomicservicenews.distribute.api;
-
-import ohos.distributedschedule.interwork.DeviceInfo;
-
-public interface SelectDeviceResultListener {
-    void onSuccess(DeviceInfo info);
-    void onFail(DeviceInfo info);
-}
--- Harmony/AtomicServiceNews/entry/src/test/java/com/example/atomicservicenews/ExampleTest.java
+++
@@ -1,9 +0,0 @@
-package com.example.atomicservicenews;
-
-import org.junit.Test;
-
-public class ExampleTest {
-    @Test
-    public void onStart() {
-    }
-}
